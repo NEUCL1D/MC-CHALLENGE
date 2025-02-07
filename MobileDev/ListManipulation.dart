7b2af9650a15481530125ddbe747f2268daaa595
Riyad G <ryad.007dz@gmail.com>
2025-02-07 21:41:23 +0100

    Update ListManipulation.dart

--- MobileDev/ListManipulation.dart
+++ MobileDev/ListManipulation.dart
@@ -1,14 +1,14 @@
 //#Challenge : List manipulation in dart
 
 void main() {
-  List<int> numbers = [1, 2, 3, 4, 5];
+  List<int> numbers = [1, 2, -3, 4, 5, 0];
   theFunction(numbers);
 }
 
 void theFunction(List<int> input) {
   int sum = 0;
-	// i used a ternary operator
-  var even = input.map((num) => num % 2 == 0 ? num * num : (sum += num) *  0).where((e) => e != 0).toList();
+  var even = input.map((num) => num % 2 == 0 ? num * num : (sum += num) == null ? null : null).where((e) => e != null).toList();
+
   var result = [even, sum];
   print("Result: $result");
 }
